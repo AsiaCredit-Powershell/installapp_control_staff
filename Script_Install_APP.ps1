f76c2554d59ab1286418ab0a81fe524c077b728a
chentsov_vs <chentsov.vyacheslav@gmail.com>
2021-06-11 14:19:18 +0300

    Переписал. Нужна проверка на леммингах

--- Script_Install_APP.ps1
+++ Script_Install_APP.ps1
@@ -1,19 +1,22 @@
+# Формируем пути до каталогов и файлов. А так же именя файлов, папок и установленных приложений 
 $LocalDir = "Controll_Staff"
 $localPath = "$env:ProgramData\$LocalDir"
 $NetPath = "\\ilc-fileserv\it\GPO\INSTALL_Control_Security"
-$FileError = "$localPath\Error.txt"
 $FileName1 = "agent_ru-5.8.2537-[192.168.1.60].msi" 
 $FileName2 = "grabber.x64.msi"
 $FirstApp = "KickidlerNode"
 $SecondApp = "Tele"
 
-$ViewApp = Get-CimInstance -ClassName Win32_Product | where {$_.name -like "$FirstApp*" -and $_.name -like "$SecondApp*"}
+# Смотрим поставились ли пакеты приложений. Если поставились - тогда ничего не делаем.
+$ViewAllApp = Get-CimInstance -ClassName Win32_Product
+$SearchApp = $ViewAllApp | Where-Object {$_.name -like "$FirstApp*" -and $_.name -like "$SecondApp*"}
 
-if ($null -ne $ViewApp)
+if ($null -ne $SearchApp)
 {
     break
 }
 
+# Если не поставились - тогда проверяем наличие файлов. Если их нет - копируем из источника.
 else 
 {
     $ViewFiles = (get-ChildItem -Path $localPath\*).FullName 
@@ -23,15 +26,16 @@ else
         Copy-Item $NetPath\* -Filter "*.msi" -Destination $localPath
     }
 
-    $ViewFirstAPP = Get-CimInstance -ClassName Win32_Product | Where-Object {$_.name -like "$FirstApp*"}
-    $ViewSecondAPP = Get-CimInstance -ClassName Win32_Product | Where-Object {$_.name -like "$SecondApp*"}
+    # Если файлы скопированы - проверяем на наличие отдельных приложений. Если одного из них нет - тогда устанавливаем по одному 
+    $SearchFirstAPP = $ViewAllApp | Where-Object {$_.name -like "$FirstApp*"}
+    $SearchSecondAPP = $ViewAllApp | Where-Object {$_.name -like "$SecondApp*"}
 
-    if ($null -eq $ViewFirstAPP)
+    if ($null -eq $SearchFirstAPP)
     {
         invoke-CimMethod -ClassName Win32_Product -MethodName Install -Arguments @{PackageLocation="$localpath\$fileName1"}
     }
 
-    elseif ($null -eq $ViewSecondAPP)
+    elseif ($null -eq $SearchSecondAPP)
     {
         invoke-CimMethod -ClassName Win32_Product -MethodName Install -Arguments @{PackageLocation="$localpath\$fileName2"}
     }
