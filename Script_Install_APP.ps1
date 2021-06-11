89eaefe26e98c61b49fcd78a6a75872278279d74
chentsov_vs <chentsov.vyacheslav@gmail.com>
2021-06-11 14:14:34 +0300

    Разбил установку приложений. Теперь требуется оптимизация

--- Script_Install_APP.ps1
+++ Script_Install_APP.ps1
@@ -4,10 +4,12 @@ $NetPath = "\\ilc-fileserv\it\GPO\INSTALL_Control_Security"
 $FileError = "$localPath\Error.txt"
 $FileName1 = "agent_ru-5.8.2537-[192.168.1.60].msi" 
 $FileName2 = "grabber.x64.msi"
+$FirstApp = "KickidlerNode"
+$SecondApp = "Tele"
 
-$ViewApp = Get-CimInstance -ClassName Win32_Product | where {$_.name -like 'Tele*' -and $_.name -like 'KickidlerNode'}
+$ViewApp = Get-CimInstance -ClassName Win32_Product | where {$_.name -like "$FirstApp*" -and $_.name -like "$SecondApp*"}
 
-if ($ViewApp -ne $null)
+if ($null -ne $ViewApp)
 {
     break
 }
@@ -21,16 +23,16 @@ else
         Copy-Item $NetPath\* -Filter "*.msi" -Destination $localPath
     }
 
-    foreach ($file in $ViewFiles){
+    $ViewFirstAPP = Get-CimInstance -ClassName Win32_Product | Where-Object {$_.name -like "$FirstApp*"}
+    $ViewSecondAPP = Get-CimInstance -ClassName Win32_Product | Where-Object {$_.name -like "$SecondApp*"}
 
-        try 
-        {        
-            invoke-CimMethod -ClassName Win32_Product -MethodName Install -Arguments @{PackageLocation="$file"}
-        }
-        
-        catch 
-        {
-            $Error | Out-File -FilePath $FileError -Encoding utf8 
-        }
+    if ($null -eq $ViewFirstAPP)
+    {
+        invoke-CimMethod -ClassName Win32_Product -MethodName Install -Arguments @{PackageLocation="$localpath\$fileName1"}
+    }
+
+    elseif ($null -eq $ViewSecondAPP)
+    {
+        invoke-CimMethod -ClassName Win32_Product -MethodName Install -Arguments @{PackageLocation="$localpath\$fileName2"}
     }
 }
